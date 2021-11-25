927b889e8c8e2b769410620a5479f23aba9ea4a7
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2021-11-25 08:02:05 +0700

    Update Dilan.js

--- commands/quotes/Dilan.js
+++ commands/quotes/Dilan.js
@@ -20,7 +20,7 @@ let dot = await cuk.json()
 J = `
 ${json}
 `
-conn.sendButtonLoc(m.chat, await ( await fetch('https://telegra.ph/file/db171335f79e5eeceefe4.jpg')).buffer(), J, userbot.packname, `Next➡️`, `dilan`, m)
+conn.sendButtonLoc(m.chat, await ( await fetch('https://telegra.ph/file/db171335f79e5eeceefe4.jpg')).buffer(), J, userbot.packname, `Dilan`, `dilan`, m)
 
 }
 }
